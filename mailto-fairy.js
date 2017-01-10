86b1f083be13a487993da2951e765961c7e7d6c4
Keith Petrino <io@kegepet.com>
2017-01-09 23:15:22 -0500

    just changed a variable name for clarity

--- mailto-fairy.js
+++ mailto-fairy.js
@@ -2,16 +2,16 @@ var mf = document.querySelectorAll('[data-mailto-fairy]');
 mf.forEach(function (item) {
   var info = item.getAttribute('data-mailto-fairy');
   info = JSON.parse(info);
-  var query = '';
+  var fields = '';
   for (var i in info) {
     if (i === 'user' || i === 'host') continue;
-    query += i + '=' + encodeURIComponent(info[i]) + '&';
+    fields += i + '=' + encodeURIComponent(info[i]) + '&';
   }
-  query = query.replace(/&$/, '');
+  fields = query.replace(/&$/, '');
   var a = document.createElement('a');
   a.id = item.id;
   a.className = item.className;
-  a.href = 'mailto:' + info.user + '@' + info.host + (query ? '?' + query : '');
+  a.href = 'mailto:' + info.user + '@' + info.host + (fields ? '?' + fields : '');
   a.appendChild(document.createTextNode(item.innerText || (info.user + '@' + info.host)));
   item.parentNode.replaceChild(a, item);
 });
